87ad6c944abf5324ef95383bc1f319923dfc5554
waitaamon <waitaamon@gmail.com>
2020-05-22 10:52:33 +0300

    updated composer, updated blog nova resource

--- app/Nova/Blog.php
+++ app/Nova/Blog.php
@@ -3,88 +3,116 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\Trix;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Blog extends Resource
 {
-    /**
-     * The model the resource corresponds to.
-     *
-     * @var string
-     */
-    public static $model = 'App\Blog';
+	/**
+	 * The model the resource corresponds to.
+	 *
+	 * @var string
+	 */
+	public static $model = 'App\Domain\Content\Blog';
 
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'id';
+	/**
+	 * The logical group associated with the resource.
+	 *
+	 * @var string
+	 */
+	public static $group = 'Content';
 
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
-    public static $search = [
-        'id',
-    ];
+	/**
+	 * The single value that should be used to represent the resource when being displayed.
+	 *
+	 * @var string
+	 */
+	public static $title = 'title';
 
-    /**
-     * Get the fields displayed by the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function fields(Request $request)
-    {
-        return [
-            ID::make()->sortable(),
-        ];
-    }
+	/**
+	 * The columns that should be searched.
+	 *
+	 * @var array
+	 */
+	public static $search = [
+		'title',
+	];
 
-    /**
-     * Get the cards available for the request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function cards(Request $request)
-    {
-        return [];
-    }
+	/**
+	 * Get the fields displayed by the resource.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	public function fields(Request $request)
+	{
+		return [
+			ID::make()->sortable(),
 
-    /**
-     * Get the filters available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function filters(Request $request)
-    {
-        return [];
-    }
+			Text::make('Title')
+				->rules('required', 'string'),
 
-    /**
-     * Get the lenses available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function lenses(Request $request)
-    {
-        return [];
-    }
+			Textarea::make('description')
+				->rules('required', 'string', 'max:200'),
 
-    /**
-     * Get the actions available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function actions(Request $request)
-    {
-        return [];
-    }
+			Image::make('Cover Image', 'cover')
+				->disk('public')
+				->disableDownload(),
+
+			Trix::make('Body')
+				->rules('required')
+				->withFiles('public'),
+
+			Boolean::make('Published')
+		];
+	}
+
+	/**
+	 * Get the cards available for the request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	public function cards(Request $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the filters available for the resource.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	public function filters(Request $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the lenses available for the resource.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	public function lenses(Request $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the actions available for the resource.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	public function actions(Request $request)
+	{
+		return [];
+	}
 }
